61b4a9e5def3169202a4de7b27c30d31f4d7295d
Miguel <tiger.tc.tc@gmail.com>
2015-03-30 11:10:01 +0200

    Aniadido visor de banners y link en el logo arreglado.

--- jss/scripts.js
+++ jss/scripts.js
@@ -1,3 +1,4 @@
+// Validador de email.
 
 function check(){
 	
@@ -22,3 +23,28 @@ function patrs(){
 	alert("Hello");
 	
 }
+
+
+// Visor de banners.
+
+var posicion = -105;
+
+function Carousel(){
+    posicion += 1;			// Saltos en px de la imagen.
+    document.getElementById('cnt-imagenes').style.top = "-" + posicion + "px";
+    if(posicion == 315){	// Han pasado 3 imágenes de 125px, reiniciar...
+        document.getElementById('cnt-imagenes').style.top = "0";
+        posicion = 0;
+        setTimeout(Carousel, 3000);
+        return;
+    }
+    
+    if(!(posicion%105)){ 	//pausa de cada imagen (125px).
+        setTimeout(Carousel, 3000);
+        return;
+    }
+    
+    runCarousel = setTimeout(Carousel, 1);	// Tiempo entre saltos de las imágenes.
+}
+
+Carousel();
